7028616fb0e0620e2228162eb58883f16a6aa36b
evanliferock <evanliferock@gmail.com>
2017-11-04 21:03:42 -0700

    Added Header file and routing

--- UI/src/index.js
+++ UI/src/index.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 import About from './about';
 import Game from './game';
+import MainPage from './main';
 import Maintenance from './maintenance'
 import LoginApp from './LoginApp'
 import './index.css';
@@ -17,10 +18,11 @@ class App extends React.Component {
         return (
             <Router>
                 <div>
-                    <Route exact path={'/'} component={Game}></Route>
+                    <Route exact path={'/'} component={MainPage}></Route>
                     <Route path={'/login'} component={LoginApp}></Route>
                     <Route path={'/about'} component={About}></Route>
                     <Route path={'/maintenance'} component={Maintenance}></Route>
+                    <Route path={'/game'} component={Game}></Route>
                 </div>
             </Router>
         );
