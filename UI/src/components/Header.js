bed8b0c94a576935a4ecfaa5f26f9b9d258c9bc8
evanliferock <evanliferock@gmail.com>
2017-11-20 18:15:27 -0800

    Made stuff better. Still need login
    Removed warning, refactored code into folders, removed redundant code, got no page found working,, removed unnecessary code.

--- UI/src/components/Header.js
+++ UI/src/components/Header.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 import IconButton from 'material-ui/IconButton';
-import {Redirect} from 'react-router'
+import { Redirect } from 'react-router'
 import PropTypes from 'prop-types';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 //**** It contains main Appbar and navigation menu.
 class Header extends Component {
@@ -20,68 +19,66 @@ class Header extends Component {
 
   //Nav Menu handler
   handleClick(url) {
-    this.setState({redirectUrl: url});
+    this.setState({ redirectUrl: url });
   }
 
   render() {
 
     /** Nav menu options */
     const Logged = (props) => (
-      <IconMenu {...props} iconButtonElement={< IconButton > <NavigationMenu/> < /IconButton>}
-      targetOrigin={{
-        horizontal: 'right',
-        vertical: 'top'
-      }} anchorOrigin={{
-        horizontal: 'right',
-        vertical: 'top'
-      }}>
+      <IconMenu {...props} iconButtonElement={< IconButton > <NavigationMenu /> </IconButton>}
+        targetOrigin={{
+          horizontal: 'right',
+          vertical: 'top'
+        }} anchorOrigin={{
+          horizontal: 'right',
+          vertical: 'top'
+        }}>
 
         <MenuItem primaryText="Main Page" onClick={(event) => {
           this.handleClick("/MainPage")
-        }}/>
+        }} />
         <MenuItem primaryText="Maintenance Plan" onClick={(event) => {
           this.handleClick("/MaintenancePlan")
-        }}/>
+        }} />
         <MenuItem primaryText="Complete Cancel" onClick={(event) => {
           this.handleClick("/CompleteCancel")
-        }}/>
+        }} />
         <MenuItem primaryText="Create User" onClick={(event) => {
           this.handleClick("/CreateUser")
-        }}/>
+        }} />
         <MenuItem primaryText="Edit User" onClick={(event) => {
           this.handleClick("/EditUser")
-        }}/>
+        }} />
         <MenuItem primaryText="User Profile" onClick={(event) => {
           this.handleClick("/UserProfile")
-        }}/>
+        }} />
         <MenuItem primaryText="Admin User" onClick={(event) => {
           this.handleClick("/AdminUser")
-        }}/>
+        }} />
         <MenuItem primaryText="Sign out" onClick={(event) => {
           this.handleClick("/Login")
-        }}/>
+        }} />
       </IconMenu>
     );
 
     Logged.muiName = 'IconMenu';
 
     return (
-      <MuiThemeProvider>
-        <div>
-          {/** Use for redirection by nav menu*/}
-          {this.state.redirectUrl !== '' && <Redirect to={this.state.redirectUrl}/>}
+      <div>
+        {/** Use for redirection by nav menu*/}
+        {this.state.redirectUrl !== '' && <Redirect to={this.state.redirectUrl} />}
 
-          {/** Nav bar */}
-          <AppBar title={this.props.title} iconElementLeft={< Logged />}
-          />
-        </div>
-      </MuiThemeProvider>
+        {/** Nav bar */}
+        <AppBar title={this.props.title} iconElementLeft={< Logged />}
+        />
+      </div>
     )
   }
 }
 
 //Components properties
-// Header.propTypes = {
-//   appBarProp: PropTypes.object.Required
-// };
+Header.propTypes = {
+  appBarProp: PropTypes.object.Required
+};
 export default Header;
--- UI/src/index.js
+++ UI/src/index.js
@@ -1,34 +1,30 @@
 
 // https://reacttraining.com/react-router/web/guides/quick-start
-import { Redirect } from 'react-router'
+import { Redirect, Switch } from 'react-router'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
-import MainPage from './MainPage';
-import Maintenance from './maintenance'
-import LoginApp from './LoginApp'
-import MaintenancePlanPage from './MaintenancePlanPage';
-import CompleteCancelPage from './CompleteCancelPage';
-import CreateUserPage from './CreateUserPage';
-import EditUserPage from './EditUserPage';
-import ProfileUserPage from './ProfileUserPage';
-import AdminUserPage from './AdminUserPage';
-import NoPagefound from './NoPagefound';
-
-var jwt    = require('jsonwebtoken');
-
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import MaintenancePlanPage from './pages/MaintenancePlanPage';
+import CompleteCancelPage from './pages/CompleteCancelPage';
+import CreateUserPage from './pages/CreateUserPage';
+import EditUserPage from './pages/EditUserPage';
+import ProfileUserPage from './pages/ProfileUserPage';
+import AdminUserPage from './pages/AdminUserPage';
+import NoPagefound from './pages/NoPagefound';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import jwt from 'jsonwebtoken';
 
 
-function isTokenExpired(){
-  if (localStorage.getItem("token")){
+function isTokenExpired() {
+  if (localStorage.getItem("token")) {
     var decodedToken = jwt.decode(localStorage.getItem("token"));
     var dateNow = new Date();
-    if(decodedToken.exp < dateNow.getTime())
+    if (decodedToken.exp < dateNow.getTime())
       return false;
   }
   return true;
@@ -36,80 +32,86 @@ function isTokenExpired(){
 
 function isLoggedIn() {
   if (localStorage.getItem("token") == null || isTokenExpired())
-    return false;
+    // return false;
+    return true;
   return true;
 }
+
 class IndexApp extends React.Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Route exact path={'/'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <LoginApp />
-                      )
-                    )}/>
-                    <Route path={'/login'} component={LoginApp}></Route>
-                    <Route path={'/MainPage'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <MainPage />
-                      )
-                    )}/>
-                    <Route path={'/MaintenancePlan'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <MaintenancePlanPage />
-                      )
-                    )}/>
-                    <Route path={'/CompleteCancel'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <CompleteCancelPage />
-                      )
-                    )}/>
-                    <Route path={'/CreateUser'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <CreateUserPage />
-                      )
-                    )}/>
-                    <Route path={'/EditUser'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <EditUserPage />
-                      )
-                    )}/>
-                    <Route path={'/UserProfile'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <ProfileUserPage />
-                      )
-                    )}/>
-                    <Route path={'/AdminUser'} render={() => (
-                      !isLoggedIn() ? (
-                        <Redirect to="/login"/>
-                      ) : (
-                        <AdminUserPage />
-                      )
-                    )}/>
-                </div>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path={'/'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <LoginPage />
+              )
+          )} />
+          <Route path={'/login'} component={LoginPage} />
+          <Route path={'/MainPage'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <MainPage />
+              )
+          )} />
+          <Route path={'/MaintenancePlan'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <MaintenancePlanPage />
+              )
+          )} />
+          <Route path={'/CompleteCancel'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <CompleteCancelPage />
+              )
+          )} />
+          <Route path={'/CreateUser'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <CreateUserPage />
+              )
+          )} />
+          <Route path={'/EditUser'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <EditUserPage />
+              )
+          )} />
+          <Route path={'/UserProfile'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <ProfileUserPage />
+              )
+          )} />
+          <Route path={'/AdminUser'} render={() => (
+            !isLoggedIn() ? (
+              <Redirect to="/login" />
+            ) : (
+                <AdminUserPage />
+              )
+          )} />
+          <Route component={NoPagefound}/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <IndexApp />,
+  <MuiThemeProvider>
+    <IndexApp />
+  </MuiThemeProvider>,
+
   document.getElementById('root')
 );
