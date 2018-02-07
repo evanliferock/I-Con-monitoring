c4506cb93141b8c041c2f1e882c0e920bda7c3a7
evanliferock <evanliferock@gmail.com>
2018-02-06 20:16:47 -0800

    Updated security. Also added the endpoint for changing one's password.

--- db_express_api/src/routes/login.js
+++ db_express_api/src/routes/login.js
@@ -3,46 +3,54 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var connection = require('../sql/db');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
 
 router.post('/', function (req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-  connection.query('SELECT * FROM USER WHERE username = ?', [username], function (error, results, fields) {
-    if (error) {
-      res.status(500).send({ "failed": "error ocurred"});
-    } else {
-      if (results.length > 0) {
-        bcrypt.compare(password, results[0].password, function (err, doesMatch) {
-          if (doesMatch) {
-            const payload = {
-              admin: true,
-              user_id:results[0].user_id,
-            };
-            var token = jwt.sign(payload, "thisIsTheSecret", {
-              expiresIn: '60m'// expires in 1 hr
-            });
-            res.status(201).send({
-              "success": "login sucessfull",
-              "token": token
-            });
-          } else {
-            res.status(401).send({
-              "success": "username and password does not match",
-              "password": password,
-              "password2": results[0].password
-            });
-          }
-        });
+  if(req.body && req.body.password && req.body.username){
+    var username = req.body.username;
+    var password = req.body.password;
+    connection.query('SELECT * FROM USER WHERE username = ?', [username], function (error, results, fields) {
+      if (error) {
+        res.status(500).send({ "failed": "error ocurred"});
       } else {
-        res.status(401).send({
-          "success": "username and password does not match"
-        });
+        if (results && results.length > 0) {
+          comparePasswords(password, results, res);
+        } else {
+          res.status(401).send({
+            "success": "username and password does not match"
+          });
+        }
       }
+    });
+  }else{
+    res.status(400).send({
+      "failed": "no username and password provided for login"
+    });
+  }
+});
+
+function comparePasswords(givenPassword, results, res){
+  bcrypt.compare(givenPassword, results[0].password, function (err, doesMatch) {
+    if (doesMatch) {
+      var payload = {
+        admin: true,
+        user_id:results[0].user_id,
+      };
+      var token = jwt.sign(payload, "thisIsTheSecret", {
+        expiresIn: '60m',// expires in 1 hr
+      });
+      res.status(201).send({
+        "success": "login sucessfull",
+        "token": token
+      });
+    } else {
+      res.status(401).send({
+        "success": "username and password does not match",
+      });
     }
   });
-});
+}
 
 module.exports = router;
--- db_express_api/src/routes/register.js
+++ db_express_api/src/routes/register.js
@@ -3,23 +3,39 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var connection = require('../sql/db');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 
 router.post('/', function (req, res) {
-  bcrypt.hash(req.body.password, 5, function (err, bcryptedPassword) {
-    req.body.password = bcryptedPassword;
-    connection.query('INSERT INTO USER SET ?', req.body, function (error, results, fields) {
+  if(req.body && req.body.password && req.body.username && req.decoded.admin){
+    connection.query('SELECT * FROM USER WHERE username = ?', [req.body.username], function (error, results, fields) {
       if (error) {
-        res.status(500).send({
-          "failed": "error ocurred"
-        });
+        res.status(500).send({ "failed": "error ocurred"});
       } else {
-        res.send({
-          "success": "user registered sucessfully"
-        });
+        if (results && results.length > 0) {
+          res.status(400).send({"failed": "username already exists"});
+        } 
       }
     });
-  });
+    var salt = bcrypt.genSaltSync(10);
+    bcrypt.hash(req.body.password, salt, function (err, bcryptedPassword) {
+      req.body.password = bcryptedPassword;
+      connection.query('INSERT INTO USER SET ?', req.body, function (error, results, fields) {
+        if (error) {
+          res.status(500).send({
+            "failed": "error ocurred"
+          });
+        } else {
+          res.send({
+            "success": "user registered sucessfully"
+          });
+        }
+      });
+    });
+  }else{
+    res.status(400).send({
+      "failed": "no username and password provided for login"
+    });
+  }
 });
 
 module.exports = router;
