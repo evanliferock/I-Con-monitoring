d9359fc3974fd8ca7f70231ff684539d14c94352
evanliferock <evanliferock@gmail.com>
2018-04-04 12:34:45 -0700

    Added the global files

---
+++ UI/src/global/paths.js
@@ -0,0 +1,16 @@
+const PATHS = {
+    LOGIN: '/Login',
+    MAIN: '/MainPage',
+    MAIN_MATCHING: '/(|MainPage)',
+    PLAN: '/MaintenancePlan',
+    UPCOMING: '/UpcomingMaintenance',
+    COMPLETE_CANCEL: '/CompleteCancel',
+    PROFILE: '/UserProfile',
+    ADMIN_PATHS: {
+        ADMIN: '/AdminUser',
+        CREATE_USER: '/CreateUser',
+        EDIT_USER: '/EditUser',
+    }
+};
+
+export default PATHS;
