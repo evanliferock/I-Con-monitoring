fbda450fe32f278836c8df1353b700d0909203b0
wrangaman <brieckers@zagmail.gonzaga.edu>
2018-04-18 09:37:55 +0000

    Slight modification to mock_sensors to help with testing and some cosmetic changes

--- db_express_api/src/sql/db.js
+++ db_express_api/src/sql/db.js
@@ -5,7 +5,7 @@ var config = {
   keepAlive: 'true',
   host: '147.222.163.1',
   username: 'seasStudent',
-  password: '$3@sDB@$e',
+  password: 'xxx',
   localPort: 27000,
   dstPort: 3306
 };
@@ -19,8 +19,8 @@ var theTunnel = tunnel(config, function(error, server) {
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   port     : '27000',
-  user     : 'brieckers',
-  password : 'brieckers49367760',
+  user     : 'yyy',
+  password : 'zzz',
   database : 'sd10_Icon_DB'
 });
 
--- mock_sensors/mock_sensors.js
+++ mock_sensors/mock_sensors.js
@@ -10,14 +10,27 @@ function getRandomInt(min, max) {
 var switches = true; // Change accordingly for testing
 var state;
 setInterval(function () {
-    var temp1 = 70;
+    var temp1 = getRandomInt(60, 67);
     var temp1c = checkTemperature(temp1);
     var post = {sensor: 'temp1', temp: temp1, color: temp1c};
 
     // Insert mock data into DB
     sql.query('INSERT INTO SENSOR_DATA SET ?', post, function(err, result) {
         if(!err) {
-            console.log('Mock Sensors Updated');
+            console.log('Mock Sensors temp1 Updated', temp1);
+        } else {
+            console.log('There was an error inserting mock state into the database');
+        }
+    }); 
+
+    var temp2 = getRandomInt(60, 67);
+    var temp2c = checkTemperature(temp1);
+    var post = {sensor: 'temp2', temp: temp2, color: temp2c};
+
+    // Insert mock data into DB
+    sql.query('INSERT INTO SENSOR_DATA SET ?', post, function(err, result) {
+        if(!err) {
+            console.log('Mock Sensors temp2 Updated', temp1);
         } else {
             console.log('There was an error inserting mock state into the database');
         }
